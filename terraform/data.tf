5df4d90c70b79eab2ba2b3cc239e11b537acf2da
megha <megha@gmail.com>
2026-06-24 06:36:04 +0530

    24-6

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,6 +2,6 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "${local.common_name}_catalogue_sg_id"
 }
 
-data "aws_ssm_parameter" "backend_alb__sg_id" {
+data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "${local.common_name}_backend_alb_sg_id"
 }
--- terraform/sg_rules.tf
+++ terraform/sg_rules.tf
@@ -3,6 +3,6 @@ resource "aws_security_group_rule" "catalogue_ingress_rule" {
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
-    security_group_id = data.aws_ssm_paramter.catalogue_sg_id.value
+    security_group_id = data.aws_ssm_parameter.catalogue_sg_id.value
     source_security_group_id = data.aws_ssm_parameter.backend_alb_sg_id.value
 }
